d65f02bc4625f263a4df0dc2aa1eb0b380a11df1
AnhMinhRW59 <114201181+AnhMinhRW59@users.noreply.github.com>
2022-09-30 10:41:31 +0700

    Update Testing_System_Assignment_4.sql

--- Testing_System_Assignment_4.sql
+++ Testing_System_Assignment_4.sql
@@ -41,46 +41,57 @@ GROUP BY D.Department_id
 HAVING COUNT(D.Department_id) > 3;
 
 -- Q5: 	viết lệnh để lấy ra danh sách câu hỏi được sử dụng trong đề thi nhiều nhất
-SELECT 
-    Q.Question_id, EQ.Exam_id, COUNT(Q.Question_id) as 'số lượng câu hỏi sử dụng trong đề thi'
+with A1 as (SELECT 
+    Q.Question_id, Q.Content, COUNT(EQ.Exam_id) AS CountE
 FROM
-    question AS Q
+    examquestion AS EQ
         RIGHT JOIN
-    examquestion AS EQ ON EQ.Question_id = Q.Question_id
-GROUP BY Q.Question_id
-ORDER BY Q.Question_id
-LIMIT 1;
+    question AS Q ON EQ.Question_id = Q.Question_id
+GROUP BY Q.Question_id)
+select *, max(CountE) as 'số đề thi được sử dụng nhiều nhất' from A1;
+
 
 -- Q6: Thống kế mỗi Category Question được sử dụng trong bao nhiêu question
-
 SELECT 
-   C.Category_id, Category_name, COUNT(Q.Category_id)
+    C.Category_id,
+    Category_name,
+    COUNT(Q.Category_id) 'số lượng câu hỏi'
 FROM
-    question AS Q
-    left JOIN
-    categoryquestion AS C ON C.Category_id = Q.Category_id
-GROUP BY Q.Category_id;
+    categoryquestion AS C
+        LEFT JOIN
+    question AS Q ON C.Category_id = Q.Category_id
+GROUP BY C.Category_id;
 
 
 -- Q7: Thống kê mỗi Question được sử dụng trong bao nhiêu exam
 SELECT 
-    EQ.Question_id, Q.Content, COUNT(EQ.Exam_id)
+    Q.Question_id, COUNT(EQ.Question_id) as 'số bài thi sử dụng câu hỏi'
 FROM
-    examquestion AS EQ
+    question AS Q
         LEFT JOIN
-    question AS Q ON Q.Question_id = EQ.Question_id
-GROUP BY EQ.Question_id;
+    examquestion AS EQ ON Q.Question_id = EQ.Question_id
+GROUP BY Q.Question_id;
 
 -- Q8: Lấy ra question có nhiều câu trả lời nhất
 SELECT 
-    A.Question_id, COUNT(A.Question_id)
+    A.Question_id, COUNT(A.Question_id) AS 'số lượng'
 FROM
     answer AS A
         LEFT JOIN
     question AS Q ON A.Question_id = Q.Question_id
 GROUP BY A.Question_id
-order by count(A.Question_id) desc
-limit 1;
+HAVING COUNT(A.Question_ID) = (SELECT 
+        MAX(CountQ)
+    FROM
+        (SELECT 
+            COUNT(A.Question_ID) AS CountQ
+        FROM
+            Answer A
+        RIGHT JOIN Question Q ON A.Question_ID = Q.Question_ID
+        GROUP BY A.Question_ID) as B);
+
+
+;
 
 -- Q9: Thống kê số lượng account trong mỗi group
 SELECT 
@@ -93,45 +104,56 @@ GROUP BY GA.Group_id
 ;
 
 -- Q10: tìm chức vụ có ít người nhất
+select * from department;
+select * from account;
 SELECT 
-    A.Department_id,
-    D.Department_name,
-    COUNT(A.Department_id) AS 'số thành viên'
+    C.id_ChucVu,
+    C.ten_ChucVu,
+    COUNT(A.id_ChucVu) AS 'số thành viên'
 FROM
     account AS A
-        LEFT JOIN
-    department AS D ON A.Department_id = D.Department_id
-GROUP BY A.Department_id
-ORDER BY COUNT(A.Department_id)
-LIMIT 1;
+        JOIN
+    chuc_vu AS C ON A.id_ChucVu = C.id_ChucVu
+GROUP BY A.id_ChucVu
+HAVING COUNT(A.id_ChucVu) = (SELECT 
+        MIN(countQ)
+    FROM
+        (SELECT 
+            COUNT(c1.id_ChucVu) AS CountQ
+        FROM
+            account AS A1
+        JOIN chuc_vu AS c1 ON A1.Department_id = c1.id_ChucVu
+        GROUP BY c1.id_ChucVu) test);
 
--- Q11: thống kê mỗi phòng ban có bao nhiêu dev, test, scrum Master, Pm
+
+-- Q11: thống kê mỗi phòng ban có bao nhiêu dev, test, scrum Master, Pm (cần xem lại)
 SELECT 
-    D.Department_id,
-    D.Department_name,
-    CV.ten_ChucVu,
-    COUNT(A.id_ChucVu) AS 'số lượng thành viên'
+    D.Department_ID,
+    D.Department_Name,
+    COUNT(CV.id_chucvu) AS 'SO LUONG'
 FROM
-    account AS A
-        LEFT JOIN
-    chuc_vu AS CV ON A.id_ChucVu = CV.id_ChucVu
-        LEFT JOIN
-    department AS D ON A.Department_id = D.Department_id
-GROUP BY D.Department_id , CV.id_ChucVu;
-
+    chuc_vu CV
+        INNER JOIN
+    `Account` A ON CV.id_ChucVu = A.id_ChucVu
+        INNER JOIN
+    Department D ON A.Department_ID = D.Department_ID
+GROUP BY A.Department_ID
+ORDER BY A.Department_ID;
 
 -- Q12: Lấy thông tin chi tiết của câu hỏi bao gồm: thông tin cơ bản của question, loại câu hỏi, ai là người tạo ra câu hỏi, câu trả lời là gì,...
 SELECT 
-    Q.Question_id,
-    Q.Content,
-    Q.Type_id,
-    Q.Creator_id,
-    A.Answer_id,
-    A.is_Correct
+    Q.Question_id 'MÃ CÂU HỎI',
+    Q.Content 'NỌI DUNG CÂU HỎI',
+    Q.Type_id 'kIỂU CÂU HỎI',
+    Q.Creator_id 'NGƯỜI TẠO RA CÂU HỎI',
+    A.Answer_id 'MÃ SỐ NGƯỜI TRẢ LỜI',
+    A.content 'NỘI DUNG CÂU TRẢ LỜI',
+    A.is_Correct 'T OR F'
 FROM
     question AS Q
-        LEFT JOIN
-    answer AS A ON Q.Question_id = A.Question_id;
+        left JOIN
+    answer AS A ON Q.Question_id = A.Question_id
+    group by Q.Question_id;
     
     -- Q13: Lấy ra số lượng câu hỏi của mỗi loại tự luận hay trắc nghiệm
     SELECT 
@@ -140,10 +162,16 @@ FROM
     question AS Q
         LEFT JOIN
     typequestion AS TQ ON Q.Type_id = TQ.Type_id
-GROUP BY TQ.Type_name;
+GROUP BY Q.Type_id;
+
 
 -- Q14: lấy ra group không có account nào
- -- do sửa nhiều dữ liệu nên xin phép bỏ qua câu hỏi này, câu 16 tương tự 
+SELECT		*
+FROM		`Group` 
+WHERE		Group_ID  NOT IN
+					(SELECT		Group_ID
+					FROM		GroupAccount);
+
 
 -- Q16 Lấy ra question không có anwser nào
 SELECT 
